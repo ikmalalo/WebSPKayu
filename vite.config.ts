4df0da5925b2278500af8d1f899b3735f2b02305
mpasd4.py <ikmalaliashari@gmail.com>
2026-08-20 09:40:38 +0800

    chore: remove base path and update alias configuration in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,10 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/WebSPKayu/',
+
   resolve: {
     alias: {
-      "@": path.resolve(process.cwd(), "./src"),
+      '@': path.resolve(process.cwd(), './src'),
     },
   },
 })
-
